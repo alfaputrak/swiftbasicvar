b58d84f38f76df9fcfa125c1f1f4188f2bb837e5
alfaputrak <alfa.putrakurnia@gmail.com>
2019-06-19 01:41:38 +0700

    Initial Commit

---
+++ swiftbasicvar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftbasicvar
+//
+//  Created by Alfa Putra on 19/06/19.
+//  Copyright © 2019 Alfa Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
